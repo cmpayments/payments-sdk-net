51fd41ebd8ea2ce1b7e5921af13f7d2c4c20c1f6
Maikel Vreugde <git@mvesign.com>
2017-10-11 10:26:21 +0200

    Removed charge validation check for usage of WireTransfer and currency

--- Source/CM.Payments.Client.Shared/Validators/ChargeValidator.cs
+++ Source/CM.Payments.Client.Shared/Validators/ChargeValidator.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-// ReSharper disable once RedundantUsingDirective
-using System.Reflection;
-using CM.Payments.Client.Model;
+﻿using CM.Payments.Client.Model;
 using FluentValidation;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CM.Payments.Client.Validators
 {
@@ -13,27 +10,12 @@ namespace CM.Payments.Client.Validators
         public ChargeValidator()
         {
             this.RuleFor(c => c.Amount).NotEmpty();
-            this.RuleFor(c => c.Currency).NotEmpty().Must(BeAValidIsoFormat).WithMessage("'Currency' must be in the ISO format.");
             this.RuleFor(c => c.Payments)
                 .Must(ContainOneItem)
                 .WithMessage("'Payments' must contain '1' payment.")
-                .Must(BeImplemented)
-                .WithMessage("'Wire Transfer' is not implemented yet.'")
                 .SetCollectionValidator(new PaymentValidator());
         }
-
-        private static bool BeAValidIsoFormat(string currency)
-        {
-            var info = new RegionInfo("NL");
-            return info.ISOCurrencySymbol == currency;
-        }
-
-        private static bool BeImplemented(IEnumerable<PaymentRequest> payments)
-        {
-            var objectType = payments?.First().GetType();
-            return !typeof(WireTransferPaymentRequest).IsAssignableFrom(objectType);
-        }
-
+        
         private static bool ContainOneItem(IEnumerable<PaymentRequest> payments)
         {
             return payments?.Count() == 1;
