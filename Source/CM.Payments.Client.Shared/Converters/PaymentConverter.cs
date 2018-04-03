53e1899d8078bdd91be3a88dc8fd394b3aff8aba
Ryan Bril <ryan.bril@cm.nl>
2018-04-03 13:01:23 +0200

    Added CanWrite false in PaymentConverter to use the default json converter instead

--- Source/CM.Payments.Client.Shared/Converters/PaymentConverter.cs
+++ Source/CM.Payments.Client.Shared/Converters/PaymentConverter.cs
@@ -12,6 +12,12 @@ namespace CM.Payments.Client.Converters
         {
             return true;
         }
+		
+		public override bool CanWrite => false;
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, [NotNull] JsonSerializer serializer)
         {
